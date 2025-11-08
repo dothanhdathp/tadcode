aaa0bf4a19af9af6c057d9f4273934bb0d37493b
dtdat <rakauhouha10@gmail.com>
2025-11-08 21:02:36 +0700

    Auto setup env for Linux

---
+++ env.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+# Define the function for confirmation
+confirm_action() {
+    while true; do
+        read -r -p "$1 (y/N): " response
+        response=${response,,}
+        case "$response" in
+            [y][e][s]|[y]) 
+                return 0 
+                ;;
+            [n][o]|[n]|"") 
+                return 1 
+                ;;
+            *)
+                echo "Invalid input. Please enter 'y' for Yes or 'n' for No."
+                ;;
+        esac
+    done
+}
+
+# Call the function and check its return status
+if confirm_action "Do you want create a new linux-venv?"; then
+    echo "Remove old python venv"
+    sudo rm -rf linux-venv
+    python3 -m venv linux-venv
+fi
+
+# main
+echo "Start python env"
+source linux-venv/bin/activate
+echo "Install requeriment"
+pip install mkdocs
+pip install pymdown-extensions
+pip install mkdocs-material
+pip install mkdocs_puml
+pip install mkdocs-network-graph-plugin
+deactivate
