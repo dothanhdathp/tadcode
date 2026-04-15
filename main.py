b24ff30f01094c51806f333caeafa7f7650acccb
dtdat <rakauhouha10@gmail.com>
2026-04-15 19:23:19 +0700

    update

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def define_env(env):
     @env.macro
     def linkslide(title, html_path):
         return f'''<a href="javascript:void(0);" onclick="openSlide('http://localhost:65000/{html_path}')">⧉ {title}</a>'''
+
+    @env.macro
+    def book(title, book, page="index.html"):
+        return f'''[{title}](http://localhost:65000/book/{book}/{page})'''
